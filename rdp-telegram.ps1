b942c5dbddf19c5908682b76690cf87378d502b0
zverev iv <mail@zverev-iv.me>
2021-05-19 13:06:48 +0300

    script refactoring

--- rdp-telegram.ps1
+++ rdp-telegram.ps1
@@ -1,11 +1,32 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 #optional
-$event = Get-WinEvent -FilterHashtable @{Logname = 'Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational'; Id = 1149 } -MaxEvents 1
+Param (
+    [Parameter(Mandatory = $false, Position = 1, HelpMessage = "Bot token")]
+    [string] $botToken = "ADD YOUR BOT TOKEN HERE",
+    [Parameter(Mandatory = $false, Position = 2, HelpMessage = "Chat id")]
+    [string] $chatId = "ADD YOUR CHAT ID HERE",
+    [Parameter(Mandatory = $false, Position = 3, HelpMessage = "Enable TLS 1.2 (only if you have connection problems with tls/ssl connection)")]
+    [bool] $enableTls12 = $false
+)
 
-$message = "RDP LOGIN`nServer: $($event.MachineName) `nFrom: $($event.Properties[2].Value) `nUser: $($event.Properties[1].Value)\\$($event.Properties[0].Value) `nOn: $($event.TimeCreated.ToString("yyyy.MM.dd HH:mm:ss"))"
+if($enableTls12) {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 #optional
+}
 
-$botToken = 'ADD YOUR BOT TOKEN HERE'
-$chatID = 'ADD YOUR CHAT ID HERE'
+$filter = @{
+    Logname = 'Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational';
+    Id = 1149
+}
 
-$telegramURI = ("https://api.telegram.org/bot" + $botToken + "/sendMessage")
-$telegramJson = ConvertTo-Json -Compress @{chat_id = $chatID; text = $message }
-$telegramResponse = Invoke-RestMethod -Uri $telegramURI -Method Post -ContentType 'application/json;charset=utf-8' -Body $telegramJson
+$evt = Get-WinEvent -FilterHashtable $filter -MaxEvents 1
+
+$msg = @"
+RDP LOGIN
+Server: $($evt.MachineName)
+From: $($evt.Properties[2].Value)
+User: $($evt.Properties[1].Value)\$($evt.Properties[0].Value)
+On: $($evt.TimeCreated.ToString("yyyy.MM.dd HH:mm:ss"))
+"@
+
+$chatUri = "https://api.telegram.org/bot$($botToken)/sendMessage"
+$body = ConvertTo-Json -InputObject @{chat_id = $chatId; text = $msg } -Compress
+
+Invoke-RestMethod -Uri $chatUri -Method Post -Body $body -ContentType 'application/json;charset=utf-8'
